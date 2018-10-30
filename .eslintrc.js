1682c314442730ead72da478a2d124c58a96fc21
keisukeawazu <keisuke.awa.basket@gmail.com>
2018-10-30 18:20:16 +0900

    設定をつか

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "env": {
+      "browser": true
+    },
+    "rules": {
+      "jsx-a11y/anchor-is-valid": [ "error", {
+          "components": [ "Link" ],
+          "specialLink": ["to"]
+      }],
+      "no-underscore-dangle": [
+        "error",
+        { "allow": ["__REDUX_DEVTOOLS_EXTENSION__"] }
+      ]
+    }
 };
