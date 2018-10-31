27caf15acd2636eabfe868f6e8dbf6f27750db2f
keisukeawazu <keisuke.awa.basket@gmail.com>
2018-10-31 10:58:20 +0900

    change place of comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,3 @@
-// const path = require('path');
-
-// const publicDir = path.join(__dirname, '/public');
-
-// module.exports = {
-//   entry: {
-//     app: './src/index.jsx',
-//   },
-//   output: {
-//     path: publicDir,
-//     publicPath: '/',
-//     filename: 'bundle.js',
-//   },
-//   // devServer: {
-//   //   contentBase: publicDir,
-//   //   port: 8080,
-//   //   publicPath: '/js/'
-//   // },
-//   devtool: 'eval-source-map',
-//   mode: 'development',
-//   module: {
-//     rules: [{
-//       test: /\.js$/,
-//       enforce: 'pre',
-//       exclude: /node_modules/,
-//       loader: 'eslint-loader',
-//     }, {
-//       test: /\.css$/,
-//       loader: ['style-loader', 'css-loader'],
-//     }, {
-//       test: /\.js$/,
-//       exclude: /node_modules/,
-//       loader: 'babel-loader',
-//     }],
-//   },
-// };
-
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -100,3 +63,41 @@ module.exports = [
     ],
   },
 ];
+
+
+// const path = require('path');
+
+// const publicDir = path.join(__dirname, '/public');
+
+// module.exports = {
+//   entry: {
+//     app: './src/index.jsx',
+//   },
+//   output: {
+//     path: publicDir,
+//     publicPath: '/',
+//     filename: 'bundle.js',
+//   },
+//   // devServer: {
+//   //   contentBase: publicDir,
+//   //   port: 8080,
+//   //   publicPath: '/js/'
+//   // },
+//   devtool: 'eval-source-map',
+//   mode: 'development',
+//   module: {
+//     rules: [{
+//       test: /\.js$/,
+//       enforce: 'pre',
+//       exclude: /node_modules/,
+//       loader: 'eslint-loader',
+//     }, {
+//       test: /\.css$/,
+//       loader: ['style-loader', 'css-loader'],
+//     }, {
+//       test: /\.js$/,
+//       exclude: /node_modules/,
+//       loader: 'babel-loader',
+//     }],
+//   },
+// };
