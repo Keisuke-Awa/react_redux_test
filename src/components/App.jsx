98c4b4f5f037bc320233e2f76d4869c6d4b5ad75
keisukeawazu <keisuke.awa.basket@gmail.com>
2018-10-31 11:18:33 +0900

    create containers directory and move Products.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 //   Switch,
 // } from 'react-router-dom';
 
-import Products from './Products';
+import Products from '../containers/Products';
 
 const App = () => (
   <div>
